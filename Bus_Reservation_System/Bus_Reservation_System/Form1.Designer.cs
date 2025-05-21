08ea13a402598bea6650ee441969bfe55afc6d28
Dinusha Madusanka <144506606+dinusha24madusanka@users.noreply.github.com>
2025-05-21 10:25:18 +0530

    Bus Reservation System
    Test

---
+++ Bus_Reservation_System/Bus_Reservation_System/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Bus_Reservation_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(737, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Bus_Reservation_System";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Passenger Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Destination:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Numbers of Seats:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Travel Date:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(427, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "City A",
+            "City B",
+            "City c"});
+            this.comboBox1.Location = new System.Drawing.Point(214, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(427, 24);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(214, 165);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(427, 22);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Book Ticket";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Cancel Reservation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "View Reservations";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(214, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(427, 22);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Contact Number:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(71, 309);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(570, 84);
+            this.listBox1.TabIndex = 17;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox2.Location = new System.Drawing.Point(214, 126);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(427, 24);
+            this.comboBox2.TabIndex = 18;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(588, 231);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 23);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Search Reservations";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Bus_Reservation_System/Bus_Reservation_System/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bus_Reservation_System
+{
+    public partial class Form1 : Form
+    {
+        private List<string> reservations = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (reservations.Count == 0)
+            {
+                MessageBox.Show("No reservations to display.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // Display each reservation in a more readable format
+            int index = 1;
+            foreach (string reservation in reservations)
+            {
+                // Parse the reservation string
+                string[] parts = reservation.Split(new[] { "PassengerName:", "Destination:", "NumberOfSeats:", "TravelDate:", "ContactNumber:" },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length >= 5)
+                {
+                    string passengerName = parts[0].Trim();
+                    string destination = parts[1].Trim();
+                    string seats = parts[2].Trim();
+                    string date = parts[3].Trim();
+                    string contact = parts[4].Trim();
+
+                    // Format the display string for better readability
+                    string displayText = $"#{index}: {passengerName} - {destination} ({seats} seats) on {date}";
+                    listBox1.Items.Add(displayText);
+                }
+                else
+                {
+                    // If the reservation data is corrupted, display as is
+                    listBox1.Items.Add($"#{index}: {reservation}");
+                }
+
+                index++;
+            }
+
+            // Optional: Add ability to view details of selected reservation
+            if (listBox1.Items.Count > 0)
+            {
+                listBox1.SelectedIndex = 0;
+                MessageBox.Show("Select a reservation to view details or delete.", "View Reservations",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0 && listBox1.SelectedIndex < reservations.Count)
+            {
+                string reservation = reservations[listBox1.SelectedIndex];
+                string[] parts = reservation.Split(new[] { "PassengerName:", "Destination:", "NumberOfSeats:", "TravelDate:", "ContactNumber:" },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length >= 5)
+                {
+                    string passengerName = parts[0].Trim();
+                    string destination = parts[1].Trim();
+                    string seats = parts[2].Trim();
+                    string date = parts[3].Trim();
+                    string contact = parts[4].Trim();
+
+                    // Populate the form fields with the selected reservation details
+                    textBox2.Text = passengerName;
+                    comboBox1.Text = destination;
+                    comboBox2.Text = seats;
+                    dateTimePicker1.Value = DateTime.Parse(date);
+                    textBox3.Text = contact;
+                }
+            }
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string passengerName = textBox2.Text;
+            string destination = comboBox1.SelectedItem.ToString();
+            string numberOfSeats = comboBox2.SelectedItem.ToString();
+            string travelDate =dateTimePicker1.Value.ToShortDateString();
+            string contactNumber = textBox3.Text;
+
+            if (string.IsNullOrWhiteSpace(passengerName) || 
+                string.IsNullOrWhiteSpace(destination) || 
+                string.IsNullOrWhiteSpace(numberOfSeats) || 
+                string.IsNullOrWhiteSpace(travelDate))
+            {
+                MessageBox.Show("All fields are required to book!!!", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                string reservation = $"PassengerName:{passengerName}" +
+                    $"Destination:{destination} NumberOfSeats:{numberOfSeats}" +
+                    $"TravelDate:{travelDate} ContactNumber:{contactNumber}";
+                reservations.Add(reservation);
+                listBox1.Items.Add(reservation);
+
+                MessageBox.Show("Reservation is Successfully created!!!", "Successfull",
+                    MessageBoxButtons.OK);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >=0)
+            {
+                reservations.RemoveAt(listBox1.SelectedIndex);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                MessageBox.Show("Reseravation cleared Successfully!!!");
+                textBox2.Text = "";
+                textBox3.Text = "";
+                comboBox1.Text = "";
+                comboBox2.Text = "";
+                dateTimePicker1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Reservation is not found");
+            }
+            textBox2.Text = "";
+            textBox3.Text = "";
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            dateTimePicker1.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string searchTerm = textBox1.Text.Trim().ToLower();
+
+            listBox1.Items.Clear();
+
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                MessageBox.Show("Please enter a search term.", "Search", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var matchedReservations = reservations
+                .Where(res => res.ToLower().Contains(searchTerm))
+                .ToList();
+
+            if (matchedReservations.Count == 0)
+            {
+                MessageBox.Show("No matching reservations found.", "Search", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            int index = 1;
+            foreach (var reservation in matchedReservations)
+            {
+                string[] parts = reservation.Split(new[] { "PassengerName:", "Destination:", "NumberOfSeats:", "TravelDate:", "ContactNumber:" },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length >= 5)
+                {
+                    string passengerName = parts[0].Trim();
+                    string destination = parts[1].Trim();
+                    string seats = parts[2].Trim();
+                    string date = parts[3].Trim();
+                    string contact = parts[4].Trim();
+
+                    string displayText = $"#{index}: {passengerName} - {destination} ({seats} seats) on {date}";
+                    listBox1.Items.Add(displayText);
+                }
+                else
+                {
+                    listBox1.Items.Add($"#{index}: {reservation}");
+                }
+
+                index++;
+            }
+
+            MessageBox.Show($"Found {matchedReservations.Count} result(s) for '{searchTerm}'", "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
+
